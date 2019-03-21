b307727938e25dbaf27caa423b5c5d243492bfee
WebD01 <liangu@protonmail.com>
2019-03-20 20:44:12 -0400

    Add set command

---
+++ src/commands/mod/set.ts
@@ -0,0 +1,25 @@
+import { Command } from "discord-akairo";
+import { Message } from 'discord.js'
+import { ToikoClient } from "../../client";
+
+export default class extends Command {
+    constructor() {
+        super('set', {
+            aliases: ['set'],
+            args: [
+                { id: 'settingName', type: 'lowercase' },
+                { id: 'settingValue', match: 'rest' }
+            ],
+            channelRestriction: 'guild',
+            description: 'Sets the given setting',
+            userPermissions: ['MANAGE_GUILD']
+        })
+
+    }
+    public exec({ guild, channel }: Message, { settingName, settingValue }: any): Promise<Message | Message[]> {
+        const client = this.client as ToikoClient
+        client.settings.set(guild.id, settingName, settingValue)
+        return channel.send(`${settingName} has been set to ${settingValue}.`)
+
+    }
+}
