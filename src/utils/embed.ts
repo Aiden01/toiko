e8af014c82f74366e952fb0ecaea8bd895072e27
WebD01 <liangu@protonmail.com>
2019-03-23 10:16:04 -0400

    feat(embed): Add color

--- src/utils/embed.ts
+++ src/utils/embed.ts
@@ -1,6 +1,7 @@
 import { Command } from 'discord-akairo'
-import { Client, GuildMember, User } from 'discord.js'
+import { Client, GuildMember, RichEmbedOptions, User } from 'discord.js'
 import * as R from 'ramda'
+import { EMBEDS_COLOR } from '../../config.json'
 
 /**
  * Builds an embed with default fields
@@ -8,36 +9,35 @@ import * as R from 'ramda'
 export const buildEmbed = async (
 	user: User | GuildMember,
 	client: Client,
-	restOptions
+	restOptions: RichEmbedOptions
 ) => {
 	const { displayAvatarURL, username } = await client.fetchUser(user.id)
+
 	return {
 		...restOptions,
 		author: {
 			iconURL: displayAvatarURL,
 			name: username,
 		},
+		color: parseInt(EMBEDS_COLOR.slice(1), 16),
 		timestamp: new Date(),
 	}
 }
 /**
  * Returns the help embed for the given command
  */
-export const buildCommandHelp = ({
-	args,
-	description,
-	aliases,
-	userPermissions,
-	id,
-}: Command) => {
+export const buildCommandHelp = (
+	{ args, description, aliases, userPermissions, id }: Command,
+	user: User | GuildMember,
+	client: Client
+) => {
 	const getArguments = R.isEmpty(args)
 		? R.always('None')
 		: R.compose(
 				R.join('\n'),
 				R.map(R.prop('id'))
 		  )
-
-	return {
+	return buildEmbed(user, client, {
 		description,
 		fields: [
 			{
@@ -51,5 +51,5 @@ export const buildCommandHelp = ({
 			},
 		],
 		title: id,
-	}
+	})
 }
