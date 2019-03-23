2da5d8cd89fe10204cb35afd269007769a3c9b36
WebD01 <liangu@protonmail.com>
2019-03-23 00:11:48 -0400

    fix(warn): Make command mod only

--- src/commands/mod/warn.ts
+++ src/commands/mod/warn.ts
@@ -12,7 +12,9 @@ export default class extends Command {
                 { type: 'string', id: 'reason', match: 'rest' }
             ],
             category: 'mod',
+            channelRestriction: 'guild',
             description: 'Warns a user',
+            userPermissions: ['MANAGE_MESSAGES'],
         })
     }
 
