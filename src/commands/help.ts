2b2d6cb6a97be70d7653f7c7a0e78eac0dc7129a
WebD01 <liangu@protonmail.com>
2019-03-24 12:13:05 -0400

    test

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -39,7 +39,7 @@ export default class extends Command {
 
 			const commandsEmbed = await buildEmbed(author, this.client, {
 				description:
-					'Type $help <command> to get more information about a command',
+					'tteeest Type $help <command> to get more information about a command',
 				fields: commands,
 			})
 
