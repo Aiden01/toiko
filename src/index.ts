2c7818fbac809173a055951554c758dca26bb50b
WebD01 <liangu@protonmail.com>
2019-03-24 09:53:38 -0400

    prod: set up docker-compose file

--- src/index.ts
+++ src/index.ts
@@ -9,19 +9,32 @@ import {
 import { ToikoClient } from './client'
 
 const client = new ToikoClient({ ownerID: ['337364150080503809'] })
-/* tslint:disable */
-createConnection({
-	database: POSTGRES_DATABASE,
-	entities: [__dirname + '/entity/*.ts'],
-	host: 'localhost',
-	password: POSTGRES_PASSWORD,
-	port: 5432,
-	synchronize: true,
-	type: 'postgres',
-	username: POSTGRES_USER,
-})
-	.then(connection => {
-		client.start(TOKEN, connection).then(() => console.log('Client is logged'))
-	})
+async function main() {
+	let retries = 5
+	while(retries) {
+		try {
+			/* tslint:disable */
+			const connection = await createConnection({
+				database: "toiko",
+				entities: [__dirname + '/entity/*.ts'],
+				host: 'db',
+				password: "postgres",
+				port: 5432,
+				synchronize: true,
+				type: 'postgres',
+				username: "postgres",
+			})
+			await client.start(TOKEN, connection)
+			console.log('Client is logged')
+			break
+		} catch(e) {
+		  console.error(e)
+		  retries--
+		  console.log(`${retries} retries left.`)
+		  await new Promise(resolve => setTimeout(resolve, 5000))
+	    }
+	} 
 
-	.catch(error => console.log(error))
+}
+
+main()
