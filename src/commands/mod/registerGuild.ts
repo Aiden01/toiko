896ee94a41349c676d25e0e45e5e0a5eea69fca5
aiden01 <liangu@protonmail.com>
2019-04-01 21:15:33 -0400

    feat: Add register guild command

--- src/commands/mod/registerGuild.ts
+++ src/commands/mod/registerGuild.ts
@@ -1,43 +1,29 @@
 import { Command } from 'discord-akairo';
 import { Message } from 'discord.js';
 import { ToikoClient } from '../../client';
-import { createMacro, getGuild } from '../../utils/db';
+import { createGuild, getGuild } from '../../utils/db';
 
 export default class extends Command {
 	constructor() {
 		super('registerGuild', {
-			aliases: ['prefix', 'setprefix'],
-			args: [{ id: 'prefix', type: 'string' }],
+			aliases: ['registerGuild'],
 			category: 'mod',
 			description: {
-				content: 'Sets the prefix for the bot',
-				example: 'prefix !',
-				usage: 'prefix <new prefix>',
+				content: 'Registers the guild in the database',
 			},
 			userPermissions: ['MANAGE_GUILD'],
 		});
 	}
 
-	public async exec(
-		{ guild, channel }: Message,
-		{ prefix }: any
-	): Promise<Message | Message[]> {
+	public async exec({ guild, channel }: Message): Promise<Message | Message[]> {
 		const client = this.client as ToikoClient;
-		if (!prefix) {
-			return channel.send(
-				`Insufficient arguments. Type ${
-					this.handler.prefix
-				}help prefix for more information.`
-			);
-		}
 		try {
 			const server = await getGuild(guild.id, client.database);
-			if (!server) {
-				return channel.send("Your guild wasn't found in the database.");
+			if (server) {
+				return channel.send('Your guild is already registered.');
 			}
-			server.prefix = prefix;
-			await server.save();
-			return channel.send(`The prefix is not ${server.prefix}`);
+			await createGuild(guild.id, client.database);
+			return channel.send(`The guild is now registered.`);
 		} catch (e) {
 			return channel.send(`An error occurred: ${e}`);
 		}
