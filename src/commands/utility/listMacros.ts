0cdc2ba1f9a41d9fbfbfe1e1b8b3e2e65db101ee
aiden01 <liangu@protonmail.com>
2019-04-01 20:44:01 -0400

    fix(listmacro): Display prefix dynamically

--- src/commands/utility/listMacros.ts
+++ src/commands/utility/listMacros.ts
@@ -1,9 +1,9 @@
-import { Command } from 'discord-akairo'
-import { Message } from 'discord.js'
-import { ToikoClient } from '../../client'
-import { getMacrosInCategory } from '../../utils/command'
-import { getAllMacros } from '../../utils/db'
-import { buildEmbed } from '../../utils/embed'
+import { Command } from 'discord-akairo';
+import { Message } from 'discord.js';
+import { ToikoClient } from '../../client';
+import { getMacrosInCategory } from '../../utils/command';
+import { getAllMacros } from '../../utils/db';
+import { buildEmbed } from '../../utils/embed';
 
 export default class extends Command {
 	constructor() {
@@ -11,7 +11,7 @@ export default class extends Command {
 			aliases: ['listmacros', 'macros'],
 			category: 'utility',
 			description: 'Lists all available macros',
-		})
+		});
 	}
 
 	public async exec({
@@ -19,36 +19,38 @@ export default class extends Command {
 		channel,
 		author,
 	}: Message): Promise<Message | Message[]> {
-		const client = this.client as ToikoClient
+		const client = this.client as ToikoClient;
 		try {
-			const macros = await getAllMacros(guild.id, client.database)
+			const macros = await getAllMacros(guild.id, client.database);
 			if (macros.length < 1) {
 				return channel.send(
-					'No macro available for this guild. Use $addmacro to add one.'
-				)
+					`No macro available for this guild. Use ${
+						this.handler.prefix
+					}addmacro to add one.`
+				);
 			}
-			const categories = [...new Set(macros.map(macro => macro.category))]
+			const categories = [...new Set(macros.map(macro => macro.category))];
 			const categoryFields = await Promise.all(
 				categories.map(async category => {
 					const categoryMacros = await getMacrosInCategory(
 						{ guildId: guild.id, category },
 						client.database
-					)
+					);
 					return {
 						name: category,
 						value: categoryMacros.map(macro => macro.name).join(', '),
-					}
+					};
 				})
-			)
+			);
 
 			const embed = await buildEmbed(author, client, {
 				fields: categoryFields,
 				title: 'Available macros',
-			})
+			});
 
-			return channel.send({ embed })
+			return channel.send({ embed });
 		} catch (e) {
-			return channel.send(`An error occurred: ${e}`)
+			return channel.send(`An error occurred: ${e}`);
 		}
 	}
 }
