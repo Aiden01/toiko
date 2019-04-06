d6f95f47c2b2ae37fcd8e1ff1028f48b0607c2e6
aiden01 <liangu@protonmail.com>
2019-04-06 10:52:07 -0400

    fix: Do not create multiple connections

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import 'reflect-metadata';
 import { ToikoClient } from './client';
-import { connect } from './utils/db';
 import { loadConfig, getConfig } from './utils/config';
+import { createConnection } from 'typeorm';
 
-(async function main() {
+(async () => {
 	try {
 		await loadConfig('.env');
 		const client = new ToikoClient({
 			ownerID: ['337364150080503809', '481284371312279573'],
 		});
-		const connection = await connect({
+		const connection = await createConnection({
 			database: getConfig('DATABASE_NAME'),
-			entities: [__dirname + '/entity/*.ts'],
+			entities: [__dirname + '/entity/*'],
 			host: getConfig('DATABASE_HOST', 'localhost'),
 			password: getConfig('DATABASE_PASSWORD'),
 			port: getConfig('DATABASE_PORT', 5432),
